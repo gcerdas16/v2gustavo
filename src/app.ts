9bf0b2fdc7349553137958bbc0326d82bfbe83c4
gcerdas16 <gcerdas16@gmail.com>
2025-03-09 22:44:50 -0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import { join } from 'path'
+import { createBot, createProvider, createFlow, addKeyword, utils } from '@builderbot/bot'
+import { MemoryDB as Database } from '@builderbot/bot'
+import { BaileysProvider as Provider } from '@builderbot/provider-baileys'
+import { config } from './config'
+import templates from './templates'
+import { providerBaileys, providerMeta } from './provider'
+
+
+const PORT = config.PORT;
+
+const main = async () => {
+    const adapterFlow = templates;
+    let adapterProvider;
+    if (config.provider === "meta") {
+        adapterProvider = providerMeta;
+    } else if (config.provider === "baileys") {
+        adapterProvider = providerBaileys;
+    } else {
+        console.log("ERROR: Falta agregar un provider al .env")
+    }
+
+
+    const adapterDB = new Database();
+
+    const { httpServer } = await createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    });
+
+    httpServer(+PORT);
+};
+
+main();
